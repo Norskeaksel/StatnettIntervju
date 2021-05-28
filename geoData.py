0a85ecbf8769682144c9f0ef109c694f602a38cd
Aksel <ahsorbye@gmail.com>
2021-05-28 10:38:04 +0200

    Read from api first

--- geoData.py
+++ geoData.py
@@ -3,9 +3,9 @@ import geopandas as gpd
 
 def addGeoData(countryData):
     url = "https://opendata.arcgis.com/datasets/a21fdb46d23e4ef896f31475217cbb08_1.geojson"
-    # world = gpd.read_file(url)
-    # world.to_file("backupWorld.geojson", driver="GeoJSON")
-    world = gpd.read_file("backupWorld.geojson")
+    world = gpd.read_file(url)
+    world.to_file("backupWorld.geojson", driver="GeoJSON")
+    # world = gpd.read_file("backupWorld.geojson")
     world = world.drop(columns=['OBJECTID'])
     world = world.rename(columns={"CNTRY_NAME": "Country"})
     combined_gdf = world.merge(countryData, on='Country', how='left').dropna()
